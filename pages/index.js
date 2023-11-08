0f9ee7ac5d1957269f6535dbe96784ac06ee48bd
nkjain92 <nk.jain92@live.com>
2023-11-08 16:31:04 +0530

    login ui updated further

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,46 @@
 // pages/index.js
+import { useEffect, useState } from 'react'; // Import useState
 import { signIn, useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.css'; // Make sure this path is correct
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const [currentText, setCurrentText] = useState(0);
+  const textArray = [
+    'Empower your conversation with AI.',
+    'Experience the future of chatbots.',
+    'Engage with smart AI technology.',
+  ]; // Replace texts when you want to change the message
 
+  // Redirect to the main chat screen if logged in
   useEffect(() => {
-    // Redirect to the main chat screen if logged in
     if (status === 'authenticated') {
       router.push('/main-chat-screen');
     }
-  }, [session, status, router]);
+  }, [status, router]);
+
+  // Text rotation effect
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentText(prevIndex => (prevIndex + 1) % textArray.length);
+    }, 2000); // Change text every 2 seconds
+
+    return () => clearInterval(intervalId);
+  }, [textArray.length]);
 
   if (status === 'loading') {
     return <div>Loading...</div>; // Or any loading indicator you prefer
   }
 
-  if (session) {
-    return null; // Or a loading indicator as the useEffect hook will handle redirection
-  }
-
   return (
-    <div className={styles.loginContainer}>
-      <div className={styles.loginContent}>
-        <h1 className={styles.title}>ChatGot</h1>
-        <p className={styles.subtitle}>Chat Freely, Got Every AI Assistants Here for You</p>
-        <button onClick={() => signIn()} className={styles.signInButton}>
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <p className={styles.dynamicText}>{textArray[currentText]}</p>
+      </div>
+      <div className={styles.rightSide}>
+        <button onClick={() => signIn('google')} className={styles.signInButton}>
           Sign in with Google
         </button>
       </div>
--- pages/main-chat-screen.js
+++ pages/main-chat-screen.js
@@ -7,44 +7,31 @@ import { useRouter } from 'next/router';
 export default function MainChatScreen() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const [input, setInput] = useState('');
+  const [chatLog, setChatLog] = useState([{ role: 'assistant', content: 'How can I help you?' }]);
+  const chatLogRef = useRef(null);
 
-  // Redirect if not logged in or while loading the session
   useEffect(() => {
+    // Redirect if not logged in or while loading the session
     if (status === 'loading') return; // Do nothing while loading
     if (status !== 'authenticated') {
       router.push('/'); // Redirect to login page if not authenticated
     }
   }, [status, router]);
 
-  // Early return for loading state
-  if (status === 'loading') {
-    return <p>Loading...</p>; // Show a loading message while checking for session
-  }
-
-  // Early return if there is no session
-  if (!session) {
-    return null; // Protect the page from being rendered if no session
-  }
-
-  const [input, setInput] = useState('');
-  const [chatLog, setChatLog] = useState([
-    {
-      role: 'assistant',
-      content: 'How can I help you?',
-    },
-  ]);
-
-  function clearChat() {
-    setChatLog([]);
-  }
-
-  const chatLogRef = useRef(null);
-
   useEffect(() => {
     if (chatLogRef.current) {
       chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight;
     }
   }, [chatLog]);
+
+  if (status === 'loading') {
+    return <p>Loading...</p>; // Show a loading message while checking for session
+  }
+
+  if (!session) {
+    return null; // Protect the page from being rendered if no session
+  }
 
   async function handlesubmit(e) {
     e.preventDefault();
@@ -60,14 +47,17 @@ export default function MainChatScreen() {
       body: JSON.stringify({ content: input }),
     });
     const data = await response.json();
-
     setChatLog(chatLog => [...chatLog, { role: 'assistant', content: data.content }]);
+  }
+
+  function clearChat() {
+    setChatLog([]);
   }
 
   return (
     <div className='App'>
       <header>
-        Welcome {session.user.name || session.user.email}, thanks for using our app.{' '}
+        Welcome {session.user.name || session.user.email}, thanks for using our app.
         <button onClick={() => signOut()}>Logout</button>
       </header>
       <aside className='sidemenu'>
@@ -85,12 +75,12 @@ export default function MainChatScreen() {
         <div className='chat-input-holder'>
           <form onSubmit={handlesubmit}>
             <input
-              type='text' // Specify the type
+              type='text'
               value={input}
               onChange={e => setInput(e.target.value)}
               className='chat-input-text-area'
               placeholder='Type your prompt here'
-              autoFocus // Optional, to focus on load
+              autoFocus
             />
           </form>
           <div className='disclaimer'>
