8521049e2005abd720e6433385e9599c324c7af2
nomadic.barbarian <sahan0123@gmail.com>
2023-11-10 10:54:40 +0530

    commented out code for callback functions in next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 // pages/api/auth/[...nextauth].ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
@@ -16,52 +16,51 @@ export default NextAuth({
   ],
   // Specify the adapter to use for database operations.
   // The default is TypeORM, but we'll use the Prisma Adapter here.
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET!,
-  callbacks: {
-    signIn: async ({ user, account, profile }) => {
-      // Here you can handle sign-in events and create or update user records
-      if (account && profile) {
-        // You may want to store additional information from the profile like avatar, email etc.
-        try {
-          // Check if the user exists in your database
-          let existingUser = await prisma.user.findUnique({
-            where: { email: profile.email },
-          });
+  // callbacks: {
+  //   signIn: async ({ user, account, profile }) => {
+  //     // Here you can handle sign-in events and create or update user records
+  //     if (account && profile) {
+  //       // You may want to store additional information from the profile like avatar, email etc.
+  //       try {
+  //         // Check if the user exists in your database
+  //         let existingUser = await prisma.user.findUnique({
+  //           where: { email: profile.email },
+  //         });
 
-          if (!existingUser) {
-            // Create a new user with the profile info
-            existingUser = await prisma.user.create({
-              data: {
-                name: profile.name,
-                email: profile.email,
-                image: profile.image,
-                sub: profile.sub,
-              },
-            });
-          }
+  //         if (!existingUser) {
+  //           // Create a new user with the profile info
+  //           existingUser = await prisma.user.create({
+  //             data: {
+  //               // name: profile.name!,
+  //               email: profile.email ?? "default@email.com",
+  //               // image: profile.image!,
+  //             },
+  //           });
+  //         }
 
-          // You can perform additional actions here if needed
+  //         // You can perform additional actions here if needed
 
-          return true; // Sign-in successful
-        } catch (error) {
-          console.error("Error during signIn", error);
-          return false; // Sign-in failed
-        }
-      }
+  //         return true; // Sign-in successful
+  //       } catch (error) {
+  //         console.error("Error during signIn", error);
+  //         return false; // Sign-in failed
+  //       }
+  //     }
 
-      // For other providers, you'd have similar logic
-      return true; // If we don't have additional checks, just return true
-    },
-    jwt: async ({ token, user, account, profile, isNewUser }) => {
-      // This is called whenever a JWT is created (i.e., at sign in)
-      if (user) {
-        token.uid = user.id;
-      }
-      return token;
-    },
-  },
-  // Enable debug messages in the console if you're having problems
-  debug: true,
+  //     // For other providers, you'd have similar logic
+  //     return true; // If we don't have additional checks, just return true
+  //   },
+  //   // jwt: async ({ token, user, account, profile, isNewUser }) => {
+  //   //   // This is called whenever a JWT is created (i.e., at sign in)
+  //   //   if (user) {
+  //   //     token.uid = user.id;
+  //   //   }
+  //   //   return token;
+  //   // },
+  // },
+  // // Enable debug messages in the console if you're having problems
+  // debug: true,
   // Configure other custom settings if necessary
 });
