348ffa55d2a63bc2d2edc7c65544ea69bbd810c7
nomadic.barbarian <sahan0123@gmail.com>
2023-11-12 15:43:26 +0530

    migration done

--- README.md
+++ README.md
@@ -34,3 +34,11 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## create db
+
+npm install prisma
+There is a file named schema.prisma. Whenever we create new DBs, we add the db details in that file
+When you create an API, and you want to route API response to db, then you import prismaclient and prisma adapter and capture all the datapoints
+
+## Download postgresql
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,9 @@
 // pages/api/auth/[...nextauth].ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
@@ -12,55 +11,62 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    // ...add more providers here
   ],
-  // Specify the adapter to use for database operations.
-  // The default is TypeORM, but we'll use the Prisma Adapter here.
-  // adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET!,
-  // callbacks: {
-  //   signIn: async ({ user, account, profile }) => {
-  //     // Here you can handle sign-in events and create or update user records
-  //     if (account && profile) {
-  //       // You may want to store additional information from the profile like avatar, email etc.
-  //       try {
-  //         // Check if the user exists in your database
-  //         let existingUser = await prisma.user.findUnique({
-  //           where: { email: profile.email },
-  //         });
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      // Check if the user exists in the database
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: user.email,
+        },
+      });
 
-  //         if (!existingUser) {
-  //           // Create a new user with the profile info
-  //           existingUser = await prisma.user.create({
-  //             data: {
-  //               // name: profile.name!,
-  //               email: profile.email ?? "default@email.com",
-  //               // image: profile.image!,
-  //             },
-  //           });
-  //         }
+      if (existingUser) {
+        // User already exists, sign-in allowed
+        return true;
+      } else {
+        // User doesn't exist yet, create a new user entry in the database
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            // Other user fields if necessary
+          },
+        });
+        return true;
+      }
+    },
+    async session({ session, token }) {
+      // Fetch additional user details and include them in the session
+      const user = await prisma.user.findUnique({
+        where: {
+          id: token.sub, // `sub` is the user ID that is used in NextAuth.js JWT
+        },
+      });
 
-  //         // You can perform additional actions here if needed
+      if (user) {
+        session.user = {
+          ...session.user,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+      }
 
-  //         return true; // Sign-in successful
-  //       } catch (error) {
-  //         console.error("Error during signIn", error);
-  //         return false; // Sign-in failed
-  //       }
-  //     }
-
-  //     // For other providers, you'd have similar logic
-  //     return true; // If we don't have additional checks, just return true
-  //   },
-  //   // jwt: async ({ token, user, account, profile, isNewUser }) => {
-  //   //   // This is called whenever a JWT is created (i.e., at sign in)
-  //   //   if (user) {
-  //   //     token.uid = user.id;
-  //   //   }
-  //   //   return token;
-  //   // },
-  // },
-  // // Enable debug messages in the console if you're having problems
-  // debug: true,
-  // Configure other custom settings if necessary
+      return session;
+    },
+    // ... other callbacks
+  },
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    // ... other custom pages
+  },
 });
